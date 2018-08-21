09a761efd817cc5d4487797f2e0939b66a81e8a0
Tom King <design.tomking@gmail.com>
2018-08-20 18:38:08 -0600

    Get random film from props and pass to return jsx

--- src/components/BackgroundScroll/index.js
+++ src/components/BackgroundScroll/index.js
@@ -3,11 +3,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './BackgroundScroll.css'
 
-const BackgroundScroll = ({episode, openingCrawl, releaseDate, title}) => {
+const BackgroundScroll = ({films}) => {
+  const randomFilmIndex = () => (Math.random()*films.length)+0.5<<0;
+  const currentFilm = films[randomFilmIndex()];
+  const { episode, openingCrawl, releaseDate, title } = currentFilm;
+
   return (
-    <section class="star-wars">
-      <div class="crawl">
-        <div class="title">
+    <section className="star-wars">
+      <div className="crawl">
+        <div className="title">
           <p className="Background_Scroll__EPISODE">Episode {episode}</p>
           <h1 className="Background_Scroll__TITLE">{title}</h1>
         </div>
