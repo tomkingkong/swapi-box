0f2c78ea67a8925a195dfcae453f1dd645644199
sojurner <pykim0591@gmail.com>
2018-08-21 18:55:56 -0600

    remove extra space

--- src/components/PeopleList/PeopleList.js
+++ src/components/PeopleList/PeopleList.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import axios from "axios";
+
 import ContentCard from "../ContentCard/ContentCard";
 
 const PeopleList = ({ people, markAsFavorite }) => {
