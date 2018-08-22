26df6885eabe4873fb159073c5a3f3a1f835336e
Tom King <design.tomking@gmail.com>
2018-08-22 12:43:00 -0600

    Add getdata function describe block in app test

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -18,5 +18,35 @@ describe('App', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  describe('getData', () => {
+    let mockEvent;
+    let mockResults;
+    let wrapper;
+    let FetchApi;
+
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+      mockEvent = { target: { textContent: "people" } };
+      mockResults = [ 
+        { name: 'Luke Skywalker',
+          species: 'Human',
+          homeworld: 'Tatooine',
+          population: '200000' 
+        },
+        { name: 'C-3PO',
+          species: 'Droid',
+          homeworld: 'Tatooine',
+          population: '200000' 
+        },
+        { name: 'R2-D2',
+          species: 'Droid',
+          homeworld: 'Naboo',
+          population: '4500000000' 
+        } 
+      ];
+    //   window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+    //     json: () => Promise.resolve(mockResults)
+    //   }))
+    // });
 
 });
