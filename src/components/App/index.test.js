d5301eed063928946c914c07ecc45228b351b92f
sojurner <pykim0591@gmail.com>
2018-08-22 17:11:19 -0600

    app test

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,101 +1,112 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
+import React from "react";
 
 import NavBar from "../Navigation/NavBar";
 import ContentRoute from "../Navigation/ContentRoute";
 import { FetchApi } from "../Fetch/FetchApi";
 import BackgroundScroll from "../BackgroundScroll";
-import App from './index.js';
+import App from "./index.js";
+import { mockPeople } from "../../MockData/MockPeople";
 
-describe('App', () => {
+describe("App", () => {
   let wrapper;
+  let mockEvent;
+  let mockResults;
+  let FetchApi;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    localStorage.clear();
     wrapper = shallow(<App />);
+    await wrapper.instance().componentDidMount;
   });
 
-  it.skip('should match snapshot with default states', () => {
+  it("should match snapshot with default states", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  describe('getData', () => {
-    let mockEvent;
-    let mockResults;
-    let wrapper;
-    let FetchApi;
-
+  describe("getData", () => {
     beforeEach(() => {
       wrapper = shallow(<App />);
-      mockEvent = { target: { textContent: "people" } };
-      mockResults = [ 
-        { name: 'Luke Skywalker',
-          species: 'Human',
-          homeworld: 'Tatooine',
-          population: '200000' 
+      mockEvent = { target: { name: "people" } };
+      mockResults = [
+        {
+          name: "Luke Skywalker",
+          species: "Human",
+          homeworld: "Tatooine",
+          population: "200000"
         },
-        { name: 'C-3PO',
-          species: 'Droid',
-          homeworld: 'Tatooine',
-          population: '200000' 
+        {
+          name: "C-3PO",
+          species: "Droid",
+          homeworld: "Tatooine",
+          population: "200000"
         },
-        { name: 'R2-D2',
-          species: 'Droid',
-          homeworld: 'Naboo',
-          population: '4500000000' 
-        } 
+        {
+          name: "R2-D2",
+          species: "Droid",
+          homeworld: "Naboo",
+          population: "4500000000"
+        }
       ];
-    //   window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-    //     json: () => Promise.resolve(mockResults)
-    //   }))
-    // });
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          json: () => Promise.resolve(mockPeople)
+        })
+      );
+    });
 
-    it('should call FetchApi', async () => {
-      await wrapper.instance().getData(mockEvent)
+    it("should call FetchApi", async () => {
+      await wrapper.instance().getData(mockEvent, 1);
       expect(fetch).toHaveBeenCalled();
     });
 
-    it('should set correct state with the value passed', async () => {
+    it("should set correct state with the value passed", async () => {
+      await wrapper.instance().getData(mockEvent, 1);
+
+      expect(wrapper.state("people").length).toEqual(10);
+    });
+
+    it("should set correct error state if value returns failed fetch", async () => {
+      window.fetch = jest
+        .fn()
+        .mockImplementation(() => Promise.reject(new Error("failed")));
       await wrapper.instance().getData(mockEvent);
-      // console.log(wrapper.state())
-      expect(wrapper.state('people').length).toEqual(3);
+      expect(wrapper.state("errorStatus")).toEqual("failed");
     });
 
-    it('should set correct error state if value returns failed fetch', async () => {
-      window.fetch = jest.fn().mockImplementation(() => Promise.reject(new Error('failed')));
-      await wrapper.instance().getData(mockEvent);
-      expect(wrapper.state('errorStatus')).toEqual('failed');
+    it("should call setButtonPressed", () => {
+      wrapper.instance().setButtonPressed = jest.fn();
+      wrapper.instance().getData(mockEvent);
+      expect(wrapper.instance().setButtonPressed).toHaveBeenCalled();
     });
 
-    it('should call setButtonPressed', () => {
-      wrapper.instance().setButtonPressed = jest.fn()
-      wrapper.instance().getData(mockEvent);      
-      expect(wrapper.instance().setButtonPressed).toHaveBeenCalled()
-    });
-
-    it.skip('should run fetch only if state is empty or not null', async () => {
+    it("should run fetch only if state is empty or not null", async () => {
       const people = [{}, {}];
-      wrapper.setState({ people})
-      expect(wrapper.state('people').length).toEqual(2);
+      wrapper.setState({ people });
+      expect(wrapper.state("people").length).toEqual(2);
       await wrapper.instance().getData(mockEvent);
-      expect(wrapper.state('people').length).toEqual(2);      
+      expect(wrapper.state("people").length).toEqual(10);
     });
 
-  });
+    it("should return if alreadyHasData is true and not a new page", async () => {
+      const people = [{}, {}];
+      const expected = people;
+      await wrapper.setState({ people });
+      await wrapper.instance().getData(mockEvent, 1);
+      expect(wrapper.state("people")).toEqual(expected);
+    });
 
+    it("should not return if alreadyHasData is true and a new page exists", async () => {
+      const people = [{}, {}];
+      wrapper.setState({ people });
+      await wrapper.instance().getData(mockEvent, 2);
+      expect(wrapper.state("people").length).toEqual(10);
+    });
 
-  it.skip('should ', () => {
+    describe();
+    it.skip("should ", () => {});
 
-  });
+    // it.skip("should ", () => {});
 
-  it.skip('should ', () => {
-
-  });
-
-  it.skip('should ', () => {
-
-  });
-
-  it.skip('should ', () => {
-
+    // it.skip("should ", () => {});
   });
 });
