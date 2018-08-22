d89bc30990ad543d8b280dacf5db6a11f2a5272b
sojurner <pykim0591@gmail.com>
2018-08-22 07:31:34 -0600

    add if block favorite

--- src/components/ContentCard/index.js
+++ src/components/ContentCard/index.js
@@ -5,13 +5,15 @@ import "./ContentCard.css";
 
 const ContentCard = ({ card, toggleFavorites }) => {
   const content = Object.keys(card).map((key, index) => {
-    return key === "name" ? (
-      <h1 key={key + index}>{card[key]}</h1>
-    ) : (
-      <p key={key + index}>
-        {key}: {card[key]}
-      </p>
-    );
+    if (key !== "favorite") {
+      return key === "name" ? (
+        <h1 key={key + index}>{card[key]}</h1>
+      ) : (
+        <p key={key + index}>
+          {key} : {card[key]}
+        </p>
+      );
+    }
   });
   return (
     <article
