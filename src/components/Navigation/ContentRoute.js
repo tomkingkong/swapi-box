7fe2c125b43bbeb840e33826c21a52fe69e35e2a
Tom King <design.tomking@gmail.com>
2018-08-22 07:31:57 -0600

    Create a saved favorites key of strings and pass to cardcontainers

--- src/components/Navigation/ContentRoute.js
+++ src/components/Navigation/ContentRoute.js
@@ -4,34 +4,35 @@ import { Switch, Route } from "react-router-dom";
 import { CardContainer } from "../CardContainer";
 
 export default ({ planets, people, vehicles, toggleFavorites, favorites }) => {
+  const savedFavorites = favorites.map(favorite => favorite.name);
   return (
     <Switch>
       <Route
         exact
         path="/planets"
         render={() => (
-          <CardContainer data={planets} toggleFavorites={toggleFavorites} />
+          <CardContainer data={planets} toggleFavorites={toggleFavorites} savedFavorites={savedFavorites}/>
         )}
       />
       <Route
         exact
         path="/people"
         render={() => (
-          <CardContainer data={people} toggleFavorites={toggleFavorites} />
+          <CardContainer data={people} toggleFavorites={toggleFavorites} savedFavorites={savedFavorites}/>
         )}
       />
       <Route
         exact
         path="/vehicles"
         render={() => (
-          <CardContainer data={vehicles} toggleFavorites={toggleFavorites} />
+          <CardContainer data={vehicles} toggleFavorites={toggleFavorites} savedFavorites={savedFavorites}/>
         )}
       />
       <Route
         exact
         path="/favorites"
         render={() => (
-          <CardContainer data={favorites} toggleFavorites={toggleFavorites} />
+          <CardContainer data={favorites} toggleFavorites={toggleFavorites} savedFavorites={savedFavorites}/>
         )}
       />
     </Switch>
