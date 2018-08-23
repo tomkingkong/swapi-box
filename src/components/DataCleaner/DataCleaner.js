37f71d0d7377643e81a26796d1cae27bff893f1a
sojurner <pykim0591@gmail.com>
2018-08-23 16:11:56 -0600

    datacleaned

---
+++ src/components/DataCleaner/DataCleaner.js
@@ -0,0 +1,40 @@
+export const filmScrape = film => {
+  const modifiedObj = {
+    title: film.title,
+    openingCrawl: film.opening_crawl,
+    episode: film.episode_id,
+    releaseDate: film.release_date
+  };
+  return modifiedObj;
+};
+
+export const peopleScrape = (result, species, homeworld) => {
+  const modifiedObj = {
+    name: result.name,
+    species: species.name,
+    homeworld: homeworld.name,
+    population: homeworld.population
+  };
+  return modifiedObj;
+};
+
+export const planetScrape = (result, residents) => {
+  const modifiedObj = {
+    name: result.name,
+    populaton: result.population,
+    terrain: result.terrain,
+    climate: result.climate,
+    residents: residents
+  };
+  return modifiedObj;
+};
+
+export const vehicleScrape = vehicle => {
+  const modifiedObj = {
+    name: vehicle.name,
+    model: vehicle.model,
+    class: vehicle.vehicle_class,
+    numberof_passengers: vehicle.passengers
+  };
+  return modifiedObj;
+};
