a831cc2e74eab2fff75b6038b95cf0818c35a51a
sojurner <pykim0591@gmail.com>
2018-08-23 16:01:43 -0600

    remove white space

--- src/components/BackgroundScroll/index.test.js
+++ src/components/BackgroundScroll/index.test.js
@@ -3,20 +3,19 @@ import React from "react";
 import { BackgroundScroll } from "./index";
 
 describe("BackgroundScroll", () => {
-  
   it("should match snapshot with data object passed", () => {
     let mockFilm = {
       title: "The Empire Strikes Back",
-      openingCrawl: "wooooorrrrds", 
-      episode: 5, 
+      openingCrawl: "wooooorrrrds",
+      episode: 5,
       releaseDate: "1980-05-17"
-    }
+    };
     const wrapper = shallow(<BackgroundScroll {...mockFilm} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it("should match snapshot without data object passed", () => {
-    let mockFilm = {}
+    let mockFilm = {};
     const wrapper = shallow(<BackgroundScroll {...mockFilm} />);
     expect(wrapper).toMatchSnapshot();
   });
