4fb1e13529f8740aea543d75e0f75ca7ad70385c
Tom King <design.tomking@gmail.com>
2018-08-23 08:18:12 -0600

    Add describe block to nav test file

--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -2,3 +2,22 @@ import React from "react";
 
 import { NavBar } from "./NavBar.js";
 import { ContentRoute } from "./ContentRoute.js";
+
+describe("NavBar", () => {
+  let navWrapper;
+  let getData;
+  let activeButton;
+  let favorites;
+
+  beforeEach(() => {
+    getData = jest.fn()
+    activeButton = 'people'
+    favorites = []
+    navWrapper = shallow(
+    <NavBar
+      getData={getData}
+      activeButton={activeButton}
+      favorites={favorites}
+    />)
+  })
+  
