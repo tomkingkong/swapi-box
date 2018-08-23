c7447445e631f1d1057b5259789bd23b44995e45
Tom King <design.tomking@gmail.com>
2018-08-23 08:07:19 -0600

    Add describe block for cardcontainer

--- src/components/CardContainer/index.test.js
+++ src/components/CardContainer/index.test.js
@@ -2,3 +2,26 @@ import React from "react";
 
 import { CardContainer } from "./index";
 
+describe("CardContainer", () => {
+  let wrapper;
+  let mockData;
+  let toggleFavorites;
+  let savedFavorites;
+  let handlePage;
+
+  beforeEach(() => {
+    mockData = [{name: "Luke Skywalker", species: "Human", homeworld: "Tatooine", population: "200000", favorite: false}]
+    toggleFavorites = jest.fn()
+    savedFavorites = []
+    handlePage = jest.fn()
+
+    wrapper = shallow(
+    <CardContainer 
+      data={mockData}
+      handlePage={handlePage} 
+      toggleFavorites={toggleFavorites}
+      savedFavorites={savedFavorites}
+    />);
+
+  })
+
