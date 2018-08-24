a1959d4fd0acaa0d60f55779e93084dde44bea9b
Tom King <design.tomking@gmail.com>
2018-08-24 10:00:03 -0600

    Add homepage component

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const HomePage = (props) => {
+  return (
+    <div>
+      <h1>Welcome to SWAPI Box</h1>
+      <h4>Click through the links above to learn about the Star Wars universe!</h4>
+    </div>
+  );
+};
