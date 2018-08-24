74ebd89e0a471fb542270e70e9cd2b09e5423e21
Tom King <design.tomking@gmail.com>
2018-08-24 17:06:15 -0600

    Add header css

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to SWAPI Box</h1>
+    <div className="header"> 
+      <h1>Welcome</h1>
       <h4>Click through the links above to learn about the Star Wars universe!</h4>
     </div>
   );
