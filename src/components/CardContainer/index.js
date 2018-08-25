ab695540068ea9e6f3ffead8dad5f66f90098fba
Tom King <design.tomking@gmail.com>
2018-08-24 18:22:53 -0600

    add gif loading screen

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -17,7 +17,7 @@ export const CardContainer = ({
         <p className="empty-fav">These are not the favorites you are looking for...</p>
       </section>);
   }
-  if (!data) return (<section className="card_container">{}</section>);
+  if (!data) return (<section className="card_container"><img src="https://media2.giphy.com/media/10MKHgkZMDlQ4M/giphy.gif" width="100px" height="100px"/></section>);
   
   const cards = data.map((card, index) => {
     card.favorite = savedFavorites.includes(card.name);
