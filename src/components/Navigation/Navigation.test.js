4fcab742f0d752dfe3bc665f220997e8321bc4dc
Tom King <design.tomking@gmail.com>
2018-08-25 14:11:26 -0600

    Remove activebutton from props

--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -7,17 +7,14 @@ import { ContentRoute } from "./ContentRoute.js";
 describe("NavBar", () => {
   let navWrapper;
   let getData;
-  let activeButton;
   let favorites;
 
   beforeEach(() => {
     getData = jest.fn();
-    activeButton = "people";
     favorites = [];
     navWrapper = shallow(
       <NavBar
         getData={getData}
-        activeButton={activeButton}
         favorites={favorites}
       />
     );
