022f805b48d14068cf201068b2ca7310e6fa73cd
Tom King <design.tomking@gmail.com>
2018-09-20 10:23:41 -0600

    Add alt tag to loading img

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -17,7 +17,10 @@ export const CardContainer = ({
         <p className="empty-fav">These are not the favorites you are looking for...</p>
       </section>);
   }
-  if (!data) return (<section className="card_container loading"><img src="https://media2.giphy.com/media/10MKHgkZMDlQ4M/giphy.gif" width="100px" height="100px"/></section>);
+  if (!data) return (
+  <section className="card_container loading">
+    <img src="https://media2.giphy.com/media/10MKHgkZMDlQ4M/giphy.gif" alt="loading" width="100px" height="100px"/>
+  </section>);
   
   const cards = data.map((card, index) => {
     card.favorite = savedFavorites.includes(card.name);
